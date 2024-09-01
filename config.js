cf77e3d4015fb56b1ff6b293d4ac3a9408d0f596
darkhaker286rgt <kavi60873@gmail.com>
2024-09-01 18:16:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB || "",
+MONGODB: process.env.MONGODB || "Y7dGxApK#ILy2eaCVW-1AiO2l7mHztMM-mffdGIrm9lOqpqBryRI",
+||"SESSION ID" 
+
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
